5845103fca73711cc97a1fd52ee87f7a03efa25e
lih8697-1 <lih8697@gmail.com>
2026-03-16 12:25:19 +0900

    5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1>⭐️ 헤더 ⭐️</h1>
         {children}
       </body>
     </html>
