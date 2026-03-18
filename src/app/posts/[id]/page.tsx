54c7aa821c5de69e2e5f3c77c9b45aed7f3e8cae
lih8697-1 <lih8697@gmail.com>
2026-03-18 12:37:45 +0900

    33-1

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -12,13 +12,18 @@ export default function Detail() {
     const [post, setPost] = useState<PostDto | null>(null);
     const [postComments, setPostComments] = useState<PostCommentDto[] | null>
         (null);
+    const [isError, setIsError] = useState(false);
     const { id: postId } = useParams();
     const router = useRouter();
 
     useEffect(() => {
 
         fetchApi(`/api/v1/posts/${postId}`)
-            .then(data => setPost(data));
+            .then(data => setPost(data))
+            .catch((e) => {
+                console.log(e);
+                setIsError(true);
+            })
 
         fetchApi(`/api/v1/posts/${postId}/comments`)
             .then(setPostComments);
@@ -39,22 +44,23 @@ export default function Detail() {
 
     const deletePostComment = (commentId: number) => {
         fetchApi(`/api/v1/posts/${postId}/comments/${commentId}`, {
-          method: "DELETE",
+            method: "DELETE",
         }).then((data) => {
-          alert(data.msg);
-          
-          if (postComments === null) return;
+            alert(data.msg);
 
-          setPostComments(
-            postComments.filter((postComment) => postComment.id !== commentId)
-          );
+            if (postComments === null) return;
+
+            setPostComments(
+                postComments.filter((postComment) => postComment.id !== commentId)
+            );
         });
-      };
+    };
 
+    if (isError) return <div>문제 발생</div>
     return (
         <>
             {post === null
-                ? <div>로딩 중...</div>
+                ? <div>로딩중..</div>
                 : <div className="flex flex-col gap-8 items-center">
                     <h1>{postId}번 글 상세페이지</h1>
                     <div>
@@ -64,43 +70,56 @@ export default function Detail() {
                     <div className="flex gap-4">
                         <Link
                             href={`/posts/${post.id}/edit`}
-                            className="border-1 rounded p-2 bg-blue-500">
+                            className="border-1 rounded p-2 bg-blue-500 cursor-pointer">
                             수정</Link>
                         <button
                             onClick={() => {
                                 onDeleteHandler(post.id);
                             }}
-                            className="border-1 rounded p-2 bg-red-500">삭제</button>
+                            className="border-1 rounded p-2 bg-red-500 cursor-pointer">삭제</button>
                     </div>
-                    <h2 className="p-2">댓글 목록</h2>
-
-                    {postComments === null && <div>로딩 중...</div>}
-                    {postComments !== null && postComments.length === 0 && (
-                        <div>댓글이 없습니다.</div>
-                    )}
-
-                    {postComments !== null && postComments.length > 0 && (
-                        <ul>
-                            <ul className="flex flex-col gap-2"></ul>
-                            {postComments.map((postComment) => (
-                                <li key={postComment.id} className="flex gap-2 items-center">
-                                    <span>{postComment.id} : </span>
-                                    <span>{postComment.content}</span>
-                                    <button className="border-2 p-2 rounded">수정</button>
-                                    <button
-                                        className="border-2 p-2 rounded"
-                                        onClick={() => {
-                                            deletePostComment(postComment.id);
-                                        }}
-                                    >
-                                        삭제
-                                    </button>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
+                    <PostCommentList
+                        postComments={postComments}
+                        deletePostComment={deletePostComment}
+                    />
                 </div>
             }
+        </>
+    )
+}
+
+function PostCommentList({ postComments, deletePostComment }: {
+    postComments: PostCommentDto[] | null,
+    deletePostComment: (commentId: number) => void
+}) {
+    return (
+        <>
+            <h2 className="p-2">댓글 목록</h2>
+
+            {postComments === null && <div>Loading...</div>}
+            {postComments !== null && postComments.length === 0 && (
+                <div>댓글이 없습니다.</div>
+            )}
+
+            {postComments !== null && postComments.length > 0 && (
+                <ul className="flex flex-col gap-2">
+                    {postComments.map((postComment) => (
+                        <li key={postComment.id} className="flex gap-2 items-center">
+                            <span>{postComment.id} : </span>
+                            <span>{postComment.content}</span>
+                            <button className="border-2 p-2 rounded cursor-pointer">수정</button>
+                            <button
+                                className="border-2 p-2 rounded cursor-pointer" 
+                                onClick={() => {
+                                    deletePostComment(postComment.id);
+                                }}
+                            >
+                                삭제
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </>
     )
 }
